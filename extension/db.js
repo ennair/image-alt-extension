a4cd0779c71237ac20cbf937dee5bfc39de40a5b
yxieCap <y.a.xie@capgemini.com>
2021-12-17 14:37:52 +0100

    add api functions

---
+++ extension/db.js
@@ -0,0 +1,32 @@
+const baseURL = 'https://us-central1-altwrite-1dae9.cloudfunctions.net/webApi/rest/';
+const getURL = baseURL + 'getAlt/';
+const postURL = baseURL + 'saveAlt/';
+
+async function getAltText(filename) {
+	const urlString = `${getURL}${filename}`;
+
+	let response = await fetch(urlString, {
+		method: 'GET',
+		mode: 'cors',
+	});
+
+	let responseJson = await response.json();
+	console.log(responseJson);
+	return responseJson;
+}
+
+async function postAltText(filename, altText) {
+	const urlString = `${postURL}${filename}`;
+
+	let response = await fetch(urlString, {
+		method: 'POST',
+		mode: 'cors',
+		body: JSON.stringify({
+			imgAlt: altText,
+		}),
+	});
+
+	let responseJson = await response.json();
+	console.log(responseJson);
+	return responseJson;
+}

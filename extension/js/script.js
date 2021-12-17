69a485b4cad25657e313e48537885a0c8c90b50d
Jacobs <tijmen.jacobs@capgemini.com>
2021-12-17 11:53:13 +0100

    Pushhh

--- extension/js/script.js
+++ extension/js/script.js
@@ -1,2 +1,28 @@
-const images = document.getElementsByTagName('img');
-console.log(images);
+// const images = document.getElementsByTagName("img");
+// console.log(images);
+
+const images = document.querySelectorAll("img");
+
+let imageArray = [];
+let emptyAlt = [];
+let imagesAlt = [];
+
+const getImages = () => {
+  images.forEach((image) => {
+    image.style = "border: 8px solid red";
+    imageArray.push(image);
+  });
+};
+
+const sortImages = () => {
+  imageArray.filter((image) => {
+    const altText = image.getAttribute("alt");
+    !altText ? emptyAlt.push(image) : imagesAlt.push(image);
+  });
+};
+
+getImages();
+sortImages();
+
+console.log(emptyAlt);
+console.log(imagesAlt);
